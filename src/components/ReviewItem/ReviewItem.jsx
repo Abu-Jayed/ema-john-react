846a6ca7cc60eb5803d8d2d6a21cea075ebd6b0e
Abu Jayed <aj.webdevlopment@gmail.com>
2023-04-06 09:31:36 +0600

    first commit

---
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './ReviewItem.css'
+
+const ReviewItem = ({ product }) => {
+	const {id, name ,img, quantity, price} = product
+	return (
+		<div className='review-item'>
+			<img src={img} alt="" />
+			<div className='review-details'>
+				<p className='product-tittle'>{ name }</p>
+				<p>Price: <span className='text-orange'>${ price }</span></p>
+				<p>Order Quantity: <span className='text-orange'>{quantity}</span></p>
+			</div>
+			<button className='btn-deltete'>Delete</button>
+		</div>
+	);
+};
+
+export default ReviewItem;
